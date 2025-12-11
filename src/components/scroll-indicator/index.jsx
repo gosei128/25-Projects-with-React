cc4b0e2b3705f67d949c11712818528f1800582b
Roni Sore <ronisore236@gmail.com>
2025-12-12 01:46:16 +0800

    add scroll indicator feature

--- src/components/scroll-indicator/index.jsx
+++ src/components/scroll-indicator/index.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-
+import "./style.css";
 const ScrollIndicator = ({ url }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   const fetchData = async (getUrl) => {
     try {
@@ -12,7 +13,7 @@ const ScrollIndicator = ({ url }) => {
       const response = await fetch(getUrl);
 
       const data = await response.json();
-      console.log(data);
+
       if (data && data.products && data.products.length > 0) {
         setData(data.products);
         setLoading(false);
@@ -27,10 +28,43 @@ const ScrollIndicator = ({ url }) => {
   useEffect(() => {
     fetchData(url);
   }, [url]);
-  console.log(data, loading);
+
+  function handleScroll() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
+
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return window.removeEventListener("scroll", () => {});
+  }, []);
+  console.log(data, scrollPercentage);
   return (
     <div>
-      <h1>Scroll Indicator</h1>
+      <div className="top-container">
+        <h1>Scroll Indicator</h1>
+        <div className="scroll-progress-tracking-container">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+
       <div className="data-container">
         {data && data.length > 0
           ? data.map((dataItem) => <p>{dataItem.title}</p>)
